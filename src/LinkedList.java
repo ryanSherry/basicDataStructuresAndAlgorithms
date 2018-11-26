fb039e8939107a7d363a608d39f92e911e810b24
Ryan Sherry <rsherrycomputers@gmail.com>
2018-11-26 10:13:47 -0700

    finished LinkedList

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -29,27 +29,69 @@ public class LinkedList {
         }
     }
 
+    //inserts new node at beginning of list
+
     public void addToBeginning(int value) {
-        Node oldHead = head;
-        head = new Node(value);
-        head.next = oldHead;
+
+        //create new node
+        Node new_node = new Node(value);
+        //set new node's reference to current head
+        new_node.next = head;
+        //set new node as the head reference
+        head = new_node;
     }
 
     public void removeFromBeginning() {
         head = head.next;
     }
 
-    public void addToEnd(int value) {
-        Node n = head;
-        Node newTail = new Node(value);
-        while (n != null) {
+    public void append(int value) {
+        //1. Create new node with data
 
-            if (n.next == null) {
-                n.next = newTail;
-                break;
-            }
-            n = n.next;
+        Node newTail = new Node(value);
+
+        //2. If list is empty, make new node the head
+
+        if (head == null) {
+            head = new Node(value);
+            return;
         }
+
+        //3. Otherwise, traverse list till last node
+
+        Node last = head;
+
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = newTail;
+        return;
+    }
+
+    public void deleteNode(int key) {
+        //1. Store head node
+        Node temp = head, prev = null;
+
+        //2. If head has Node with matching key, then we make the next node the new head
+        if(temp != null && temp.item == key) {
+            head = temp.next;
+            return;
+        }
+
+        //3. Search for Node with key to be deleted. We need to keep track of the previous node
+        while (temp != null && temp.item != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        //4. If key is not present in list, return
+        if (temp == null) {
+            return;
+        }
+
+        //5. Unlink the node from the Linked List
+        prev.next = temp.next;
     }
 
     public void removeFromEnd() {
@@ -65,51 +107,40 @@ public class LinkedList {
          }
     }
 
-    public void addAtNode(int targetValue, int newValue) {
-        Node n = head;
-        Node nodeToAdd = new Node(newValue);
-        while (n.item != targetValue) {
+//    public void addAtNode(int targetValue, int newValue) {
+//        Node n = head;
+//        Node nodeToAdd = new Node(newValue);
+//        while (n.item != targetValue) {
+//
+//            if (n.next.item == targetValue) {
+//                nodeToAdd.next = n.next.next;
+//                n.next = nodeToAdd;
+//                break;
+//            }
+//            n = n.next;
+//        }
+//    }
 
-            if (n.next.item == targetValue) {
-                nodeToAdd.next = n.next.next;
-                n.next = nodeToAdd;
-                break;
-            }
-            n = n.next;
-        }
-    }
+    void insertAfter(Node target_node, int newData) {
 
-    void addAtIndex(int indexTarget, int value) {
-
-        Node n = head;
-        Node nodeToInsert = new Node(value);
-        int length = listLength();
-
-        if (indexTarget >= length || indexTarget < 0) {
-            throw new IndexOutOfBoundsException("Index outside of the bounds of the linked list");
+        //1. Check if target node is null
+        if (target_node == null) {
+            System.out.println("The given previous node cannot be null");
+            return;
         }
 
-        if (indexTarget == 0) {
-            addToBeginning(value);
-        }
+        //2. Create node with data
 
-        if (indexTarget == listLength() - 1) {
-            addToEnd(value);
-        }
+        Node new_node = new Node(newData);
 
-        for (int i = 0; i <= indexTarget; i++) {
+        //3. Link new_node to nodes after the target_node
 
-            // subtracting 1 from index to get the node before the target
-            if (i == indexTarget - 1) {
-                nodeToInsert.next = n.next;
-                n.next = nodeToInsert;
-                break;
-            }
+        new_node.next = target_node.next;
 
-            if(n.next != null) {
-                n = n.next;
-            }
-        }
+        // Link target_node to new_node
+
+        target_node.next = new_node;
+
     }
 
     int listLength() {
@@ -150,9 +181,9 @@ public class LinkedList {
 
             //add to end of list
 
-            linkedList.addToEnd(4);
+            linkedList.append(4);
 
-            linkedList.addToEnd(5);
+            linkedList.append(5);
 
             //remove from end of list
 
@@ -160,11 +191,11 @@ public class LinkedList {
 
             //add node at value
 
-            linkedList.addAtNode(3, 99);
+            linkedList.insertAfter(second, 99);
 
             //add node at index
 
-            linkedList.addAtIndex(2, 88);
+            linkedList.insertAfter(third, 88);
 
             //traverse list
 
